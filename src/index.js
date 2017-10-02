1a7a6b99c2e47a20bef6738ebe1ed9a0628f630d
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-10-02 16:37:40 +1300

    initial framework and plan

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* PROJECT PLAN
+---------------------------------------
+Force Back app based on the NZ school recess game
+See README for Outline
+---------------------------------------
+Steps
+
+1. Create grid
+2. Generate kick from random choice of distance and direction.
+3. Move players according to kick.
+4. Register score or ball out of play and reset players
+*/
